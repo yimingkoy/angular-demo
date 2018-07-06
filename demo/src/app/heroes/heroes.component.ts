b992489256ee4b085e952b7ac89dc050b2ac3609
Yiming Wang <Wang.Yiming@citrix.com>
2018-07-06 17:56:35 +0800

    heroes-2

--- demo/src/app/heroes/heroes.component.ts
+++ demo/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +12,19 @@ export class HeroesComponent implements OnInit {
     id : 1,
     name : 'windstorm'
   }
-  heroes = HEROES;
-
-  constructor() { }
+  heroes: Hero[];
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 }
