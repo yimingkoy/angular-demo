2cee0f19ef09c07aa5a45e95db731bbdbac2a8cd
yiming <yimingwa@126.com>
2018-07-10 00:20:56 +0800

    heroes-6

--- demo/src/app/hero.service.ts
+++ demo/src/app/hero.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-//import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 const httpOptions = {
@@ -21,7 +21,6 @@ export class HeroService {
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
-  
   getHeroes(): Observable<Hero[]> {
     // this.messageService.add('HeroService: fetched heroes');
     // return of(HEROES);
@@ -31,8 +30,6 @@ export class HeroService {
         catchError(this.handleError('getHeroes', []))
       );
   }
-
-  
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
@@ -42,23 +39,35 @@ export class HeroService {
     // this.messageService.add(`HeroService: fetched hero id=${id}`);
     // return of(HEROES.find(hero => hero.id === id));
   }
-  
-  updateHero (hero: Hero): Observable<any> {
+  updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`updateed hero id = ${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     );
   }
-
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id = ${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`delete hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
 
-  private handleError<T> (operation = 'operation', result?:T) {
+  private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
-    }
+    };
   }
 }
--- demo/src/app/in-memory-data.service.ts
+++ demo/src/app/in-memory-data.service.ts
@@ -13,7 +13,7 @@ export class InMemoryDataService implements InMemoryDbService {
             { id: 18, name: 'Dr IQ' },
             { id: 19, name: 'Magma' },
             { id: 20, name: 'Tornado' },
-        ]
+        ];
         return {heroes};
     }
 }
